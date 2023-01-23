33f456f0b28737cc232169f59c663b4796ce3a64
Adrian-14 <oswaldok14@gmail.com>
2023-01-23 13:02:21 -0600

    update

--- conexion.php
+++ conexion.php
@@ -1,21 +1,14 @@
 <?php 
-class ApptivaDB{    
-    private $host   ="localhost";
-    private $usuario="root";
-    private $clave  ="";
-    private $db     ="loginphp";
-    public $conexion;
-    public function __construct(){
-        $this->conexion = new mysqli($this->host, $this->usuario, $this->clave,$this->db)
-        or die(mysqli_error($conexion));
-        $this->conexion->set_charset("utf8");
-    }
 
-    //BUSCAR LOG IN
-    public function buscar($tabla, $condicion){
-        $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion") or die($this->conexion->error);
-        if($resultado)
-            return $resultado->fetch_all(MYSQLI_ASSOC);
-        return false;
-    } 
+$servername = 'localhost';
+$user = 'root';
+$pass = ' ';
+
+$conn = mysqli_connect($servername,$user,$pass);
+if(!$conn){
+    die('Error : ' .mysqli_connect_errno());
 }
+echo 'Conexion Exitosa';
+$conn->close();
+
+?>
