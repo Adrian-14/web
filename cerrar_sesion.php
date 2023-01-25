2c2ff659573bbee3fa4ebd24db909b9e3fbc579e
Adrian-14 <oswaldok14@gmail.com>
2023-01-25 12:42:41 -0600

    update

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,11 +1,5 @@
 <?php
 session_start();
-$varsesion = $_SESSION['user'];
-if($varsesion == null || $varsesion = ''){
-  echo 'Aceeso no autorizado';
-  die();
-}
 session_destroy();
 header("location:login.php");
-
 ?>
