1863b0680ca0e9b13b75a278575243a1edffd1da
Adrian-14 <oswaldok14@gmail.com>
2023-01-27 13:56:49 -0600

    update

--- login.php
+++ login.php
@@ -9,23 +9,26 @@
   <body class="text-center">
     
 <main class="form-signin w-100 m-auto">
-  <form action="validar.php" method="post">
+  <form action="validar.php" method="post" class="needs-validation" novalidate>
     <img class="mb-4" src="bootstrap-logo.svg" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Iniciar</h1>
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="user" required>
       <label for="floatingInput">Correo</label>
-      <div class="valid-feedback">Correcto</div>
-      <div class="invalid-feedback">Invalido</div>
+      <div class="valid-feedback">Excelente</div>
+      <div class="invalid-feedback">Es necesario poner Correo</div>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="clave" required>
       <label for="floatingPassword">Clave de Acceso</label>
+      <div class="valid-feedback">Excelente</div>
+      <div class="invalid-feedback">Es necesario poner Contraseña</div>
     </div>
     <button class="w-100 btn btn-lg btn-success" type="submit" value="ingresar">Iniciar</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2023</p>
     </form>
 </main>
+<script src="script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- validar.php
+++ validar.php
@@ -17,8 +17,11 @@ if($array['contar'] > 0){
     <?php
     include("login.php");
     ?>
-    <div class="alert alert-danger" role="alert">
-        Datos Incorrectos!
+    <div class="alert alert-danger d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+        <div>
+            Credenciales Incorrectas
+        </div>
     </div>
     <?php
 }
