4a34984e7346fb1b8b12ca8d1f29a7bc8d7383f9
Adrian-14 <oswaldok14@gmail.com>
2023-01-27 20:03:43 -0600

    update

--- controlpanel.php
+++ controlpanel.php
@@ -16,7 +16,7 @@ if(!isset($user)){
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -24,9 +24,63 @@ if(!isset($user)){
     <h1 class="visually-hidden">Bienvenido al Panel de control</h1>
     <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
       <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <img src="svg.png" alt="logo">
+        <img class="bi pe-none me-2" width="40" height="32" src="bootstrap-logo.svg" alt="logo">
         <span class="fs-4">Control Escolar</span>
       </a>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page"><i class="bi bi-house-fill"></i> Inicio</a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-person-vcard-fill"></i>
+          Alumnos
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-person-fill-up"></i>
+          Registro
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-person-fill-check"></i>
+          Asistencia
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-clipboard2-check-fill"></i>
+          Calificaciones
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-clipboard2-plus-fill"></i>
+          Kardex
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-signpost-2-fill"></i>
+          Disiplinas
+          </a>
+        </li>
+        <li>
+          <a href="" class="nav-link text-white">
+          <i class="bi bi-tencent-qq"></i>
+          Instructor
+          </a>
+        </li>
+      </ul>
+      <hr>
+      <div class="dropdown">
+        <a href="#" class="d-flex align-items-center tex-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="myAvatar.png" class="img-circle" width="50px" height="50px" alt="avatar">
+        </a>
+      </div>
     </div>
     
     
