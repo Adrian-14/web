302a662fb223e832adc3e4e2a5a7fc95af4ae5e8
Adrian-14 <oswaldok14@gmail.com>
2023-01-30 14:45:08 -0600

    update

--- controlpanel.php
+++ controlpanel.php
@@ -21,7 +21,8 @@ if(!isset($user)){
 </head>
 
 <body>
-  <main class="d-flex flex-nowrap">
+  <main>
+    <div class="d-flex flex-nowrap">
     <div class="d-flex flex-column p-3 text-bg-dark" style="width: 280px;">
       <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <img class="bi pe-none me-2" width="40" height="32" src="bootstrap-logo.svg" alt="logo">
@@ -97,38 +98,8 @@ if(!isset($user)){
         </ul>
       </div>
     </div>
-    <div class="b-example-divider"></div>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu" aria-labelledby="dropdownId">
-                <a class="dropdown-item" href="#">Action 1</a>
-                <a class="dropdown-item" href="#">Action 2</a>
-              </div>
-            </li>
-          </ul>
-          <form class="d-flex my-2 my-lg-0">
-            <input class="form-control me-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-    
+    </div>
+    <div class="b-example-divider"></div>    
     <div class="header">
       <div class="container-fluid">
         <div class="header-body">
