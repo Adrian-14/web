907d9d5833a0b92721808c19834a57cfff39cb52
Adrian-14 <oswaldok14@gmail.com>
2023-01-30 14:35:35 -0600

    update

--- controlpanel.php
+++ controlpanel.php
@@ -20,7 +20,7 @@ if(!isset($user)){
   <link rel="stylesheet" href="controlstyle.css">
 </head>
 
-<body cz-shortcut-listen="true">
+<body>
   <main class="d-flex flex-nowrap">
     <h1 class="visually-hidden">Bienvenido al Panel de control</h1>
     <div class="d-flex flex-column p-3 text-bg-dark" style="width: 280px;">
@@ -99,6 +99,37 @@ if(!isset($user)){
       </div>
     </div>
     <div class="b-example-divider"></div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownId">
+                <a class="dropdown-item" href="#">Action 1</a>
+                <a class="dropdown-item" href="#">Action 2</a>
+              </div>
+            </li>
+          </ul>
+          <form class="d-flex my-2 my-lg-0">
+            <input class="form-control me-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    
     <div class="header">
       <div class="container-fluid">
         <div class="header-body">
@@ -114,66 +145,7 @@ if(!isset($user)){
         </div>
       </div>
     </div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 col-lg-6 col-xl">
-          <div class="card">
-            <div class="card-body">
-              <div class="row align-items-center gx-0">
-                <div class="col">
-                  <h6 class="text-uppercase text-muted mb-2">
-                    Alumnos
-                  </h6>
-                  <span class="h2 mb-0">20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-lg-6 col-xl">
-          <div class="card">
-            <div class="card-body">
-              <div class="row align-items-center gx-0">
-                <div class="col">
-                  <h6 class="text-uppercase text-muted mb-2">
-                    Alumnos
-                  </h6>
-                  <span class="h2 mb-0">20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-lg-6 col-xl">
-          <div class="card">
-            <div class="card-body">
-              <div class="row align-items-center gx-0">
-                <div class="col">
-                  <h6 class="text-uppercase text-muted mb-2">
-                    Alumnos
-                  </h6>
-                  <span class="h2 mb-0">20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-lg-6 col-xl">
-          <div class="card">
-            <div class="card-body">
-              <div class="row align-items-center gx-0">
-                <div class="col">
-                  <h6 class="text-uppercase text-muted mb-2">
-                    Alumnos
-                  </h6>
-                  <span class="h2 mb-0">20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
   </main>
   <footer>
     <!-- place footer here -->
