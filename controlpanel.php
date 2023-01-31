cf37c4ec3c387a8694d4c0e02a0eaf5b6e7037b5
Adrian-14 <oswaldok14@gmail.com>
2023-01-31 16:04:35 -0600

    update

--- controlpanel.php
+++ controlpanel.php
@@ -21,7 +21,7 @@ if (!isset($user)) {
 <body>
   <header>
     <div class="container-fluid">
-      <div class="row flex-nowrap">
+      <div class="row flex-nowrap scrollarea">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
           <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -35,12 +35,12 @@ if (!isset($user)) {
                 </a>
               </li>
               <li>
-                <a href="#" class="nav-link align-middle px-0">
-                <i class="bi bi-people-fill me-2"></i><span class="ms-1 d-none d-sm-inline text-white">Alumnos</span>
+                <a href="#" class="nav-link text-white">
+                <i class="bi bi-people-fill me-2"></i><span class="ms-1 d-none d-sm-inline">Alumnos</span>
                 </a>
               </li>
               <li>
-                <a href="registro.php" class="nav-link px-0 align-middle">
+                <a href="registro.php" class="nav-link">
                 <i class="bi bi-calendar3 me-2"></i><span class="ms-1 d-none d-sm-inline text-white">Registro</span></a>
               </li>
               <li>
