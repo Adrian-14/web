73a3cdb1ef20a825937a0d2620c9508e9bc26ab7
Adrian-14 <oswaldok14@gmail.com>
2023-02-17 15:37:11 -0600

    update

--- index.php
+++ index.php
@@ -94,8 +94,8 @@ if (!isset($user)) {
     <div id="sidebar">
       <div class="sidebar__title">
         <div class="sidebar__img">
-          <img src="assets/logo.png" alt="logo" />
-          <h1>Codersbite</h1>
+          <img src="bootstrap-logo.svg" alt="logo" width="40px" height="32px"/>
+          <h1>Control Panel</h1>
         </div>
         <i onclick="closeSidebar()" class="fa fa-times" id="sidebarIcon" aria-hidden="true"></i>
       </div>
@@ -103,58 +103,28 @@ if (!isset($user)) {
       <div class="sidebar__menu">
         <div class="sidebar__link active_menu_link">
           <i class="fa fa-home"></i>
-          <a href="#">Dashboard</a>
+          <a href="index.php">Inicio</a>
         </div>
-        <h2>MNG</h2>
+        <h2>Menu</h2>
         <div class="sidebar__link">
           <i class="fa fa-user-secret" aria-hidden="true"></i>
-          <a href="#">Admin Management</a>
+          <a href="alumnos.php" class="nav-link">Alumnos</a>
         </div>
         <div class="sidebar__link">
           <i class="fa fa-building-o"></i>
-          <a href="#">Company Management</a>
+          <a href="#">Instructor</a>
         </div>
         <div class="sidebar__link">
           <i class="fa fa-wrench"></i>
-          <a href="#">Employee Management</a>
+          <a href="#">Registro</a>
         </div>
         <div class="sidebar__link">
           <i class="fa fa-archive"></i>
-          <a href="#">Warehouse</a>
+          <a href="#">Asistencia</a>
         </div>
         <div class="sidebar__link">
           <i class="fa fa-handshake-o"></i>
-          <a href="#">Contracts</a>
-        </div>
-        <h2>LEAVE</h2>
-        <div class="sidebar__link">
-          <i class="fa fa-question"></i>
-          <a href="#">Requests</a>
-        </div>
-        <div class="sidebar__link">
-          <i class="fa fa-sign-out"></i>
-          <a href="#">Leave Policy</a>
-        </div>
-        <div class="sidebar__link">
-          <i class="fa fa-calendar-check-o"></i>
-          <a href="#">Special Days</a>
-        </div>
-        <div class="sidebar__link">
-          <i class="fa fa-files-o"></i>
-          <a href="#">Apply for leave</a>
-        </div>
-        <h2>PAYROLL</h2>
-        <div class="sidebar__link">
-          <i class="fa fa-money"></i>
-          <a href="#">Payroll</a>
-        </div>
-        <div class="sidebar__link">
-          <i class="fa fa-briefcase"></i>
-          <a href="#">Paygrade</a>
-        </div>
-        <div class="sidebar__logout">
-          <i class="fa fa-power-off"></i>
-          <a href="#">Log out</a>
+          <a href="#">Disiplinas</a>
         </div>
       </div>
     </div>
