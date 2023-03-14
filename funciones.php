a8c31b3f4a0b2c901fd037f628e78c86267cdce0
Adrian-14 <oswaldok14@gmail.com>
2023-03-14 12:38:21 -0600

    update

--- funciones.php
+++ funciones.php
@@ -1,6 +1,8 @@
 <?php
 include 'conn.php';
 
-function delete($$database,)
-
+function delete($database,$field_id, $id){
+    $sql = "delete from ".$database." where ".$field_id."=".$id."";
+    return $sql;
+}
 ?>
