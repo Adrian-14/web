bb7ac4c546533f2ec3d221d881b1033fa6dce3a8
Adrian-14 <oswaldok14@gmail.com>
2023-03-14 10:54:13 -0600

    update

--- alta.php
+++ alta.php
@@ -6,6 +6,7 @@ $foto = $_FILES['foto']['name'];
 $ruta = $_FILES['foto']['tmp_name'];
 $destino = "fotos/".$foto;
 copy($ruta,$destino);
+
 $nombre = $_POST['nombre'];
 $apellido1 = $_POST['apellido1'];
 $apellido2 = $_POST['apellido2'];
