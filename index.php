1bf6d7e6a4e363948564d9603b0dcd871f550b49
Adrian-14 <oswaldok14@gmail.com>
2023-03-15 13:55:45 -0600

    update

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ if (!isset($user)) {
                   <div class="text-center">
                     <h5 class="fw-normal mt-0 text-muted">Maestros</h5>
                     <h3 class="mt-3 mb-3 text-center">
-                      <span class="mb-0">0</span>
+                      <span class="mb-0"><?= $rowcountins ?></span>
                     </h3>
                     <p class="text-muted">Dando Clase</p>
                   </div>
